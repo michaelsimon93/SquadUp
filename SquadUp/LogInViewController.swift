8cdb5d3efc229431dfa736f3c65dc44e47fe478b
oudie33 <MichaelOudenhoven@michaels-macbook-pro.local>
2016-03-12 20:27:01 -0600

    Configured Log In and Register view controllers with custom borders. Embedded scroll views to move text fields when user is using the keyboard, keyboard offset isn't implemented yet.

--- SquadUp/LogInViewController.swift
+++ SquadUp/LogInViewController.swift
@@ -10,15 +10,69 @@ import UIKit
 
 class LogInViewController: UIViewController {
 
+    //MARK: - Properties
+    
+    @IBOutlet weak var backgroundImage: UIImageView!
+    @IBOutlet weak var basketballImage: UIImageView!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    
+    var defaultScrollViewHeight: CGFloat = 0.0
+
+    //orange color for the views
+    let orange = UIColor(red: 0.86, green: 0.49, blue: 0.19, alpha: 1.0)
+    
+    //MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        //configure the custom text field, button borders, and image views
+        configureFields()
+        configureImages()
+        
+    }
+    
+    
+    override func viewWillAppear(animated: Bool) {
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    //MARK: - Initialization
+    
+    func configureFields() {
+        //configure the custom text field and button borders
+        emailTextField.layer.borderWidth = 2.5
+        emailTextField.layer.borderColor = orange.CGColor
+        emailTextField.layer.cornerRadius = 7.0
+        
+        passwordTextField.layer.borderWidth = 2.5
+        passwordTextField.layer.borderColor = orange.CGColor
+        passwordTextField.layer.cornerRadius = 5.0
+        
+        logInButton.layer.borderWidth = 2.5
+        logInButton.layer.borderColor = UIColor.whiteColor().CGColor
+        logInButton.layer.cornerRadius = 5.0
+        logInButton.backgroundColor = orange
+        
+
+    }
+    
+    func configureImages() {
+        backgroundImage.backgroundColor = UIColor.grayColor()
+        basketballImage.backgroundColor = UIColor.blackColor()
     }
     
 
--- SquadUp/RegisterViewController.swift
+++ SquadUp/RegisterViewController.swift
@@ -10,15 +10,72 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
+    //MARK: - Properties
+    
+    @IBOutlet weak var backgroundImage: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var basketballImage: UIImageView!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPassTextField: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    //orange color for the views
+    let orange = UIColor(red: 0.86, green: 0.49, blue: 0.19, alpha: 1.0)
+    
+    
+    //MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        //configure the background view (based on device and the basketball (if necessary)
+        configureImages()
+        
+        //configure the custom text field and button borders
+        configureFields()
+        
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    //MARK: - Initialization
+    
+    func configureFields() {
+        //configure the custom text field and button borders
+        emailTextField.layer.borderWidth = 2.0
+        emailTextField.layer.borderColor = orange.CGColor
+        emailTextField.layer.cornerRadius = 5.0
+        
+        passwordTextField.layer.borderWidth = 2.0
+        passwordTextField.layer.borderColor = orange.CGColor
+        passwordTextField.layer.cornerRadius = 5.0
+        
+        confirmPassTextField.layer.borderWidth = 2.0
+        confirmPassTextField.layer.borderColor = orange.CGColor
+        confirmPassTextField.layer.cornerRadius = 5.0
+        
+        signUpButton.layer.borderWidth = 2.0
+        signUpButton.layer.borderColor = UIColor.whiteColor().CGColor
+        signUpButton.layer.cornerRadius = 5.0
+        signUpButton.backgroundColor = orange
+    }
+    
+    func configureImages() {
+        backgroundImage.backgroundColor = UIColor.grayColor()
+        basketballImage.backgroundColor = UIColor.blackColor()
     }
     
 
@@ -31,5 +88,13 @@ class RegisterViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    
+    //MARK: - IBActions
+    
+    @IBAction func backClicked(sender: AnyObject) {
+        //pop back to the sign in view
+        self.navigationController?.popToRootViewControllerAnimated(true)
+    }
 
 }
