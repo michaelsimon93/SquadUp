d640a246fd1e5287d204ba9a01a96e511fbf42ee
oudie33 <MichaelOudenhoven@michaels-macbook-pro.local>
2016-03-15 21:09:06 -0500

    Added table view for the open games view controller. In addition, added view controllers for creating a game and joining an already made game. Created sub directories to better manage the files associated with each tab (not sure if everyone will get these or they will only be on my computer)

---
+++ SquadUp/Open Games/SecondViewController.swift
@@ -0,0 +1,57 @@
+//
+//  SecondViewController.swift
+//  SquadUp
+//
+//  Created by Michael Oudenhoven on 3/5/16.
+//  Copyright © 2016 CS 407. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    
+    
+    
+    //MARK: - Table View Delegate Methods
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        //send the event with the segue
+        //print(indexPath.row)
+        
+        //get the event that the user clicked on and send it with the segue
+        //let toSend = events[indexPath.row]
+        //self.performSegueWithIdentifier("toEventDetailViewController", sender: toSend)
+    }
+    
+    
+    
+    
+    
+    //MARK: - Table View Data Source
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Game") as! GameTableViewCell
+        
+        return cell
+    }
+
+
+}
+
