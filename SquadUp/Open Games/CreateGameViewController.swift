baa328fc22cd7adfc1994cbf735a31c94fdc7804
michaelsimon93 <michaelsimon93@gmail.com>
2016-04-06 17:09:06 -0500

    Added to the Create Game View and CreateGameViewController

--- SquadUp/Open Games/CreateGameViewController.swift
+++ SquadUp/Open Games/CreateGameViewController.swift
@@ -10,6 +10,37 @@ import UIKit
 
 class CreateGameViewController: UIViewController {
 
+    @IBOutlet weak var DateTimePicker: UIDatePicker!
+    @IBOutlet weak var createGame_location: UITextField!
+    @IBOutlet weak var createGame_type: UITextField!
+    
+    //var newGame:[Game] = [Game]()
+    var newGame : Game!
+    var totalAllowed:Int = 0
+    
+    @IBAction func createGame_button(sender: UIButton) {
+        //need to convert the DateTimePicker
+        
+        
+        if (createGame_type == "3v3" || createGame_type == "3V3") {
+            totalAllowed = 6
+        } else if (createGame_type == "4v4" || createGame_type == "4V4") {
+            totalAllowed = 8
+        } else if (createGame_type == "5v5" || createGame_type == "5V5") {
+            totalAllowed = 10
+        }
+        
+        //newGame.setValue(_____, forKey: "time")
+        newGame.setValue(createGame_location, forKey: "location")
+        newGame.setValue(createGame_type, forKey: "gameType")
+        newGame.setValue(1, forKey: "numPlayersJoined")
+        newGame.setValue(totalAllowed, forKey: "totalPlayersAllowed")
+    }
+    
+    @IBAction func cancel_button(sender: UIButton) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
